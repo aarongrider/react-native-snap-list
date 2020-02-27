110ac3c3a19aa165dfe6820a039c52a2ffaef1dc
Annie Elequin <annie@elequin.io>
2020-02-27 14:34:49 -0600

    eh glitchy working

--- src/Example/bottomSheet/index.js
+++ src/Example/bottomSheet/index.js
@@ -12,32 +12,44 @@ import {USE_NATIVE_DRIVER} from '../config';
 
 const HEADER_HEIGHT = 50;
 const windowHeight = Dimensions.get('window').height;
-const SNAP_POINTS_FROM_TOP = [0, windowHeight * 0.5, windowHeight * 0.8];
+// const SNAP_POINTS_FROM_TOP = [0, windowHeight * 0.5, windowHeight * 0.8];
+
+const data = [1, 2, 3, 4, 5];
+const colors = ['red', 'blue', 'green', 'yellow', 'pink'];
 
 export class BottomSheet extends Component {
-  masterdrawer = React.createRef();
+  outerViewRef = React.createRef();
   drawer = React.createRef();
   drawerheader = React.createRef();
   scroll = React.createRef();
+  scrollOne = React.createRef();
+  scrollTwo = React.createRef();
+  scrollThree = React.createRef();
+
   constructor(props) {
     super(props);
-    const START = SNAP_POINTS_FROM_TOP[0];
-    const END = SNAP_POINTS_FROM_TOP[SNAP_POINTS_FROM_TOP.length - 1];
+    const START = 0;
+    const END = windowHeight;
 
-    this.state = {
-      lastSnap: END,
-    };
+    this.scrollRefs = Array(data.length).fill(React.createRef());
 
     this._lastScrollYValue = 0;
     this._lastScrollY = new Animated.Value(0);
     this._onRegisterLastScroll = Animated.event(
-      [{nativeEvent: {contentOffset: {y: this._lastScrollY}}}],
+      [
+        {
+          nativeEvent: {
+            contentOffset: {y: this._lastScrollY},
+          },
+        },
+      ],
       {useNativeDriver: USE_NATIVE_DRIVER},
     );
-    // this._lastScrollY.addListener(({value}) => {
-    //   this._lastScrollYValue = value;
-    // });
+    this._lastScrollY.addListener(({value}) => {
+      this._lastScrollYValue = value;
+    });
 
+    this._dragOffset = 0;
     this._dragY = new Animated.Value(0);
     this._onGestureEvent = Animated.event(
       [{nativeEvent: {translationY: this._dragY}}],
@@ -67,134 +79,87 @@ export class BottomSheet extends Component {
   // };
   _onHandlerStateChange = ({nativeEvent}) => {
     if (nativeEvent.oldState === State.ACTIVE) {
-      let {velocityY, translationY} = nativeEvent;
-      translationY -= this._lastScrollYValue;
-      const dragToss = 0.05;
-      const endOffsetY =
-        this.state.lastSnap + translationY + dragToss * velocityY;
+      let {translationY} = nativeEvent;
+      // translationY -= this._lastScrollYValue;
+      // console.log(
+      //   'last scroll y value',
+      //   this._lastScrollYValue,
+      //   this._lastScrollY,
+      // );
+      console.log('drag y', this._dragY._value);
+      // this._dragY.setValue(translationY);
 
-      let destSnapPoint = SNAP_POINTS_FROM_TOP[0];
-      for (let i = 0; i < SNAP_POINTS_FROM_TOP.length; i++) {
-        const snapPoint = SNAP_POINTS_FROM_TOP[i];
-        const distFromSnap = Math.abs(snapPoint - endOffsetY);
-        if (distFromSnap < Math.abs(destSnapPoint - endOffsetY)) {
-          destSnapPoint = snapPoint;
-        }
-      }
-      this.setState({lastSnap: destSnapPoint});
-      this._translateYOffset.extractOffset();
-      this._translateYOffset.setValue(translationY);
-      this._translateYOffset.flattenOffset();
+      this._dragOffset += nativeEvent.translationY;
+      this._dragY.setOffset(this._dragOffset);
       this._dragY.setValue(0);
-      Animated.spring(this._translateYOffset, {
-        velocity: velocityY,
-        tension: 68,
-        friction: 12,
-        toValue: destSnapPoint,
-        useNativeDriver: USE_NATIVE_DRIVER,
-      }).start();
+      // const dragToss = 0.05;
+      // const endOffsetY =
+      //   this.state.lastSnap + translationY + dragToss * velocityY;
+
+      // let destSnapPoint = SNAP_POINTS_FROM_TOP[0];
+      // for (let i = 0; i < SNAP_POINTS_FROM_TOP.length; i++) {
+      //   const snapPoint = SNAP_POINTS_FROM_TOP[i];
+      //   const distFromSnap = Math.abs(snapPoint - endOffsetY);
+      //   if (distFromSnap < Math.abs(destSnapPoint - endOffsetY)) {
+      //     destSnapPoint = snapPoint;
+      //   }
+      // }
+      // this.setState({lastSnap: destSnapPoint});
+      // this._translateYOffset.extractOffset();
+      // this._translateYOffset.setValue(translationY);
+      // this._translateYOffset.flattenOffset();
+      // this._dragY.setValue(0);
+      // Animated.spring(this._translateYOffset, {
+      //   velocity: velocityY,
+      //   tension: 68,
+      //   friction: 12,
+      //   toValue: destSnapPoint,
+      //   useNativeDriver: USE_NATIVE_DRIVER,
+      // }).start();
     }
   };
+
   render() {
+    console.log('scroll refs', this.scrollRefs);
     return (
       <View style={StyleSheet.absoluteFillObject} pointerEvents="box-none">
-        <Animated.View
-          style={[
-            StyleSheet.absoluteFillObject,
-            {
-              transform: [{translateY: this._translateY}],
-            },
-          ]}>
-          {/* <PanGestureHandler
-            ref={this.drawerheader}
-            simultaneousHandlers={[this.scroll, this.masterdrawer]}
-            shouldCancelWhenOutside={false}
-            onGestureEvent={this._onGestureEvent}
-            onHandlerStateChange={this._onHeaderHandlerStateChange}>
-            <Animated.View style={styles.header} />
-          </PanGestureHandler> */}
-          <PanGestureHandler
-            ref={this.drawer}
-            simultaneousHandlers={[this.scroll, this.masterdrawer]}
-            shouldCancelWhenOutside={false}
-            onGestureEvent={this._onGestureEvent}
-            onHandlerStateChange={this._onHandlerStateChange}>
-            <Animated.View
-              style={[styles.container, {backgroundColor: 'pink'}]}>
-              <NativeViewGestureHandler
-                ref={this.scroll}
-                waitFor={this.masterdrawer}
-                simultaneousHandlers={this.drawer}>
-                <Animated.ScrollView
-                  style={[
-                    styles.scrollView,
-                    {marginBottom: SNAP_POINTS_FROM_TOP[0]},
-                  ]}
-                  bounces={false}
-                  onScrollBeginDrag={this._onRegisterLastScroll}
-                  scrollEventThrottle={1}>
-                  <LoremIpsum />
-                  <LoremIpsum />
-                  <LoremIpsum />
-                </Animated.ScrollView>
-              </NativeViewGestureHandler>
-            </Animated.View>
-          </PanGestureHandler>
-          <PanGestureHandler
-            ref={this.drawer}
-            simultaneousHandlers={[this.scroll, this.masterdrawer]}
-            shouldCancelWhenOutside={false}
-            onGestureEvent={this._onGestureEvent}
-            onHandlerStateChange={this._onHandlerStateChange}>
-            <Animated.View
-              style={[styles.container, {backgroundColor: 'yellow'}]}>
-              <NativeViewGestureHandler
-                ref={this.scroll}
-                waitFor={this.masterdrawer}
-                simultaneousHandlers={this.drawer}>
-                <Animated.ScrollView
-                  style={[
-                    styles.scrollView,
-                    {marginBottom: SNAP_POINTS_FROM_TOP[0]},
-                  ]}
-                  bounces={false}
-                  onScrollBeginDrag={this._onRegisterLastScroll}
-                  scrollEventThrottle={1}>
-                  <LoremIpsum />
-                  <LoremIpsum />
-                  <LoremIpsum />
-                </Animated.ScrollView>
-              </NativeViewGestureHandler>
-            </Animated.View>
-          </PanGestureHandler>
-          <PanGestureHandler
-            ref={this.drawer}
-            simultaneousHandlers={[this.scroll, this.masterdrawer]}
-            shouldCancelWhenOutside={false}
-            onGestureEvent={this._onGestureEvent}
-            onHandlerStateChange={this._onHandlerStateChange}>
-            <Animated.View
-              style={[styles.container, {backgroundColor: 'green'}]}>
-              <NativeViewGestureHandler
-                ref={this.scroll}
-                waitFor={this.masterdrawer}
-                simultaneousHandlers={this.drawer}>
-                <Animated.ScrollView
-                  style={[
-                    styles.scrollView,
-                    {marginBottom: SNAP_POINTS_FROM_TOP[0]},
-                  ]}
-                  bounces={false}
-                  onScrollBeginDrag={this._onRegisterLastScroll}
-                  scrollEventThrottle={1}>
-                  <LoremIpsum />
-                  <LoremIpsum />
-                  <LoremIpsum />
-                </Animated.ScrollView>
-              </NativeViewGestureHandler>
-            </Animated.View>
-          </PanGestureHandler>
-        </Animated.View>
+        <PanGestureHandler
+          ref={this.outerViewRef}
+          waitFor={[...this.scrollRefs]}
+          onGestureEvent={this._onGestureEvent}
+          onHandlerStateChange={this._onHandlerStateChange}>
+          <Animated.View
+            style={[
+              StyleSheet.absoluteFillObject,
+              {
+                transform: [{translateY: this._dragY}],
+              },
+            ]}>
+            {data.map((_, i) => (
+              <PanGestureHandler
+                key={i}
+                ref={this.drawer}
+                waitFor={this.scrollRefs[i]}
+                // simultaneousHandlers={[...this.scrollRefs, this.outerViewRef]}
+                onGestureEvent={this._onGestureEvent}
+                onHandlerStateChange={this._onHandlerStateChange}>
+                <Animated.View
+                  style={[styles.container, {backgroundColor: colors[i]}]}>
+                  <NativeViewGestureHandler ref={this.scrollRefs[i]}>
+                    <Animated.ScrollView
+                      style={[styles.scrollView]}
+                      onScrollBeginDrag={this._onRegisterLastScroll}
+                      scrollEventThrottle={1}>
+                      <LoremIpsum />
+                      <LoremIpsum />
+                      <LoremIpsum />
+                    </Animated.ScrollView>
+                  </NativeViewGestureHandler>
+                </Animated.View>
+              </PanGestureHandler>
+            ))}
+          </Animated.View>
+        </PanGestureHandler>
       </View>
     );
   }
@@ -212,7 +177,7 @@ export default class Example extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    height: 300,
+    height: 400,
   },
   header: {
     height: HEADER_HEIGHT,
